ebae06b0294069e78468cba146dd1c5516f4e19c
sap_install <sap_install>
2017-01-20 10:33:42 +0100

    updated allowed payload size and increased request timout

--- config/credentials.js
+++ config/credentials.js
@@ -1,2 +1,9 @@
 secret = require('../secret.json');
 module.exports = "mssql://"+ secret.login + ":" + secret.password + "@localhost/";
+module.exports.PCM = {
+    user: secret.login,
+    password: secret.password,
+    server: 'localhost', // You can use 'localhost\\instance' to connect to named instance 
+    database: 'PCM',
+    requestTimeout: 600000
+}
--- routes/dataLoad.js
+++ routes/dataLoad.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var sql = require('mssql');
-var credentials = require('../config/credentials') + "PCM";
+var credentials = require('../config/credentials') + "PCM?requestTimeout=600000";
 
 /* GET users listing. */
 router.post('/', function(req, res, next) {
